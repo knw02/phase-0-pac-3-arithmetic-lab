04ba41217bb09f9dcc3fcf17d7ed7dfc10039df5
Kalie <kwatsonnov2002@gmail.com>
2023-10-13 14:30:03 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,42 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return n + 1;
+}
+function decrement(n) {
+    return n - 1;
+}
+function makeInt(n) {
+  const parsedInt = parseInt (n);
+  return parsedInt; 
+}
+function makeInt(n) {
+    const parsedInt = parseInt(n, 10);
+    return parsedInt;
+}
+function preserveDecimal(n) {
+    const parsedFloat = parseFloat(n);
+    return parsedFloat;
+}
